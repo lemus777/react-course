0c3ff82154281cbe8a583d17ceac6327bac23d91
Lemus <lemus777@gmail.com>
2023-08-18 19:20:51 +0200

    filtrado por categorias y búsqueda por título

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -25,6 +25,7 @@ const CheckoutSideMenu = () => {
         context.setOrder([...context.order, orderToAdd])
         context.setCartProducts([])
         context.setCount(0)
+        context.setSearchByTitle(null)
     }
 
     return (
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,30 +1,43 @@
-import { useState, useEffect } from "react"
+import { useContext } from "react"
+import { ShoppingCartContext } from "../../Context"
 import Layout from "../../Components/Layout"
 import Card from "../../Components/Card"
 import ProductDetail from "../../Components/ProductDetail"
 
 function Home() {
-  const [items, setItems] = useState(null)
+  const context = useContext(ShoppingCartContext)
 
-  useEffect(() => {
-    fetch('https://api.escuelajs.co/api/v1/products')
-      .then(response => response.json())
-      .then(data => setItems(data))
-  }, [])
-
-    return (
-      <Layout>
-        Home
-        <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
-          {
-            items?.map(item => (
-              <Card key={item.id} data={item} />
-            ))
-          }
-        </div>
-        <ProductDetail />
-      </Layout>
-    )
+  const renderView = () => {
+    if (context.filteredItems?.length > 0) {
+      return (
+        context.filteredItems?.map(item => (
+          <Card key={item.id} data={item} />
+        ))
+      )
+    } else {
+      return (
+        <div>Product don't available</div>
+      )
+    }
   }
+
+  return (
+    <Layout>
+      <div className='flex items-center justify-center relative w-80 mb-4'>
+        <h1 className='font-medium text-xl'>Exclusive Products</h1>
+      </div>
+      <input
+        type='text'
+        placeholder='Search a product'
+        className='rounded-lg border border-black w-80 p-4 mb-4 focus:outline-none'
+        onChange={(event) => context.setSearchByTitle(event.target.value)}
+      />
+      <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+        {renderView()}
+      </div>
+      <ProductDetail />
+    </Layout>
+  )
+}
   
-  export default Home
+export default Home
